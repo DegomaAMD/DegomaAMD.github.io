193efbee407ff42a1f889c29316b9cd3ae543cc3
Andria Mae Degoma <degoma.andria@gmail.com>
2023-02-12 17:49:32 +0800

    modified menu page

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const resendEmail = document.getElementById('resendEmail');
 const username = document.getElementById('username');
 const mainMenu = document.getElementById('mainMenu');
 const espressoPage = document.getElementById('espressopage');
+const nonCoffeePage = document.getElementById('nonCoffee');
+const frappeNonCoffee = document.getElementById('frappeNonCoffee');
+const frappeCoffee = document.getElementById('frappeCoffee');
+const refresherDrinks = document.getElementById('refresherDrinks');
 
 function passResetValue(){
     valueEmailPass = emailInput.value;
@@ -18,11 +22,47 @@ function usernameValue(){
     usernameVal = username.value;
     usernameInput.innerHTML = usernameVal;
 }
-function changeMainMenuToEspressoPage(){
-    mainMenu.replaceChild(espressoPage, mainMenu.childNodes[0]);
 
+// Espresso Menu
+function changeMainMenuToEspressoPage(){
+    espressoPage.setAttribute ('class', 'col-8 ms-3');
+    mainMenu.replaceWith(espressoPage, mainMenu);
 }
 function changeEspressoPageToMainMenu(){
-    espressoPage.replaceChild(mainMenu, espressoPage.childNodes[0]);
+    espressoPage.replaceWith(mainMenu, espressoPage);   
+}
+// Non-Coffee Menu
+function changeMainMenuTononCoffeePage(){
+    nonCoffeePage.setAttribute ('class', 'col-8 ms-3');
+    mainMenu.replaceWith(nonCoffeePage, mainMenu);
+}
+function changenonCoffeePageToMainMenu(){
+    nonCoffeePage.replaceWith(mainMenu, nonCoffeePage);
 
+}
+// Frappe Non-Coffee Menu
+function changeMainMenuTofrappeNonCoffee(){
+    frappeNonCoffee.setAttribute ('class', 'col-8 ms-3');
+    mainMenu.replaceWith(frappeNonCoffee, mainMenu);
+}
+function changefrappeNonCoffeeToMainMenu(){
+    frappeNonCoffee.replaceWith(mainMenu, frappeNonCoffee);
+
+}
+// Frappe Coffee Menu
+function changeMainMenuTofrappeCoffee(){
+    frappeCoffee.setAttribute ('class', 'col-8 ms-3');
+    mainMenu.replaceWith(frappeCoffee, mainMenu);
+}
+function changefrappeCoffeeToMainMenu(){
+    frappeCoffee.replaceWith(mainMenu, frappeCoffee);
+
+}
+// Refresher
+function changeMainMenuTorefresher(){
+    refresherDrinks.setAttribute('class', 'col-8 ms-3');
+    mainMenu.replaceWith(refresherDrinks, mainMenu);
+}
+function changerefresherToMainMenu(){
+    refresherDrinks.replaceWith(mainMenu, refresherDrinks);   
 }
